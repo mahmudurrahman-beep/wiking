8f82c747d8a4d3d63bfc373951bfb7ae1204ead8
Tushar_github <mahmudurrahman23yahoo@gmail.com>
2026-02-04 11:38:33 +0600

    Update settings.py

--- wiki/settings.py
+++ wiki/settings.py
@@ -104,7 +104,7 @@ USE_I18N = True
 USE_TZ = True
 
 # ====== STATIC FILES ======
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [BASE_DIR / 'encyclopedia' / 'static']
